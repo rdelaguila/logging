f9e63fac29867416f9f62555af0945f60b6c1bcd
rdelaguila <rdelaguila@gmail.com>
2023-11-26 17:59:01 +0100

    modificaciones

--- ejemplo_1.py
+++ ejemplo_1.py
@@ -19,10 +19,20 @@ if __name__ == '__main__':
         logging.config.dictConfig(yaml_config)
 
     ## Para registrar los mensajes, necesitamos un logger.  El logger tiene, por defecto, 5 niveles de criticidad
-    ## para los mensajes.
-    ## En este caso, lo hemos configurado mediante fichero de configuración. Vamos a ello.
-    ## Para cada módulo, puedo definir los registros con __name__ o script, permitiendo un control más granular.
-    logger = logging.getLogger(__name__)
+    ## para los mensajes: https://docs.python.org/3/library/logging.html#levels.
+    ## 0. NOTSET
+    ## 10. DEBUG: INFORMACIÓN DE INTERÉS PARA UN DEPURADOR
+    ## 20. INFO: CONFIRMACIÓN DE QUE LAS COSAS FUNCIONAN COMO DEBEN
+    ## 30. WARNING: ALGO INESPERADO HA OCURRIDO Y PUEDE SER CAUSA DE UN PROBLEMA EN EL FUTURO
+    ## 40. ERROR: DEBIDO A UN ERROR EL PROGRAMA NO HA FUNCIONADO COMO SE ESPERABA
+    ## 50. CRITICAL: EL PROGRAMA DEBIDO A UN ERROR NO PUEDE SEGUIR FUNCIONANDO
+
+    ## En este caso, lo hemos configurado mediante fichero de configuración y hemos establecido el nivel de criticidad en DEBUG. Vamos a analizar el fichero.
+
+    
+    ## Ojo, se pueden definir  los registros con __name__ (que se refiere al nombre del módulo en python)
+    # o script, permitiendo un control más granular.
+    logger = logging.getLogger(__name__) ## Un logger siempre se tiene que instanciar a través de getLogger
 
     logger.info("hello world")
     logger.error("fatal error")
